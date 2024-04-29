7dc92899963072df82b78b0b815ba2d74ebf03b2
민규 <xiahtic73@gmail.com>
2024-04-29 23:50:20 +0900

    add: 렌더링 data 생성

---
+++ inflearn-study-js-1/data.js
@@ -0,0 +1,72 @@
+const data = [
+  {
+    name: '베스트 플레이트',
+    price: 26900,
+    imgUrl: './assets/plate1.jpg',
+    description: '잘나가는 두 녀석이 만났습니다. 베스트 메뉴 두개의 조합!',
+    type: 'plate',
+  },
+  {
+    name: '바베큐 플레이트',
+    price: 23000,
+    imgUrl: './assets/plate2.jpg',
+    description:
+      '숯불에 직접 구워낸 치킨 바베큐와 다양한 구성을 넣은 대표 플레이트 입니다.',
+    type: 'plate',
+  },
+  {
+    name: '샐러드 스파게티',
+    price: 16500,
+    imgUrl: './assets/spaghetti1.jpg',
+    description:
+      '매일 전처리한 신선한 채소에 상큼하고 살짝 매콤한 드레싱 소스와 차가운 면이 들어간 미즈컨테이너 대표 "비벼먹는" 스파게티 입니다.',
+    type: 'spaghetti',
+  },
+  {
+    name: '토마토 스파게티',
+    price: 14500,
+    imgUrl: './assets/spaghetti2.jpg',
+    description:
+      '청량고추와 완전 숙성된 캘리포니아산 토마토를 사용하여 진하게 만든 살짝 매콤한 토마토 소스에 야채와 해물 or 불고기를 넣어 조리한 팬 스파게티 입니다.',
+    type: 'spaghetti',
+  },
+  {
+    name: '특선 반반 리조또',
+    price: 14700,
+    imgUrl: './assets/risotto1.jpg',
+    description:
+      '특제 크림소스와 토마토소스! 그리고 가운데 로제까지 3가지 맛의 특별한 리조또를 즐겨보세요.',
+    type: 'risotto',
+  },
+  {
+    name: '해물 크림 리조또',
+    price: 13500,
+    imgUrl: './assets/risotto2.jpg',
+    description: '해물과 크림소스를 사용한 스테디셀러 리조또입니다.',
+    type: 'risotto',
+  },
+  {
+    name: '버섯 크림 리조또',
+    price: 13000,
+    imgUrl: './assets/risotto3.jpg',
+    description: '버섯과 크림소스를 사용한 스테디셀러 리조또입니다.',
+    type: 'risotto',
+  },
+  {
+    name: '베이컨 포테이토 팬치즈',
+    price: 16000,
+    imgUrl: './assets/pizza1.jpg',
+    description:
+      '신선한 베이컨과 웨지컷 감자, 그리고 사이드와인더 감자를 같이 토핑하여 담백한 맛이 특징인 대표 피자입니다.',
+    type: 'pizza',
+  },
+  {
+    name: '하이파이브 팬치즈',
+    price: 18900,
+    imgUrl: './assets/pizza2.jpg',
+    description: '5가지 대표 피자 메뉴를 한번에 맛볼수 있는 올인원 피자입니다.',
+    type: 'pizza',
+  },
+];
+
+export default data;
