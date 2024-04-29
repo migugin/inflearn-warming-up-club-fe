ab3c4f2106160ad886afba7ad83051e6c1f841c5
민규 <xiahtic73@gmail.com>
2024-04-30 00:38:32 +0900

    feat: 데이터 렌더링 구현

---
+++ inflearn-study-js-1/index.js
@@ -0,0 +1,36 @@
+import data from './data.js';
+
+const navigation = document.querySelector('.navigation');
+const menuList = document.querySelector('.menu-list');
+const menuItem = document.querySelector('.menu-item');
+
+const setElementContent = (element, content) => {
+  element.innerHTML = content;
+};
+
+const setImageAttributes = (imageElement, src, altText) => {
+  imageElement.src = src;
+  imageElement.alt = altText;
+};
+
+const getContent = ({ name, imgUrl, description, price }) => {
+  const content = menuItem.cloneNode(true);
+  const [titleElement, imgContainer, descriptionElement, priceElement] =
+    content.children;
+
+  setElementContent(titleElement, name);
+  setImageAttributes(imgContainer.firstElementChild, imgUrl, `${name} 이미지`);
+  setElementContent(descriptionElement, description);
+  setElementContent(priceElement, `${price}원`);
+
+  return content;
+};
+
+const renderMenuList = menu => {
+  const renderList = menu.map(item => getContent(item));
+
+  menuList.replaceChildren(...renderList);
+};
+
+
+renderMenuList(data);
